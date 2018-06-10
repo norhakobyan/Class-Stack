eb8cf03636652b93f71843b49fb4c22413742f24
norhakobyan <39120844+norhakobyan@users.noreply.github.com>
2018-06-11 01:53:39 +0400

    Update ClassStack.cpp

--- ClassStack.cpp
+++ ClassStack.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
-const int size = 5;
-
 class Stack {
 public:
+	Stack(const int size = 5);
+	~Stack();
 	void push(int);
 	int pop();
 private:
-	int res = -1;
-	int stack[size];
+	int res;
+	int * stack;
+	int size;
 
 };
 
@@ -20,6 +21,10 @@ int main() {
 	tmp->push(3);
 	tmp->push(4);
 	tmp->push(5);
+	tmp->push(6);
+	tmp->push(7);
+	tmp->push(8);
+
 	std::cout << std::endl;
 	std::cout << "pop items " << tmp->pop() << std::endl;
 	std::cout << "pop items " << tmp->pop() << std::endl;
@@ -29,9 +34,26 @@ int main() {
 	std::cout << std::endl;
 }
 
+Stack::Stack(int size) {
+	this->size = size;
+	res = -1;
+	stack = new int[size];
+}
+
 void Stack::push(int sum) {
-	if (this->res >= size)
+	if (this->res == 30) {
+		std::cout << "Stack overflow" << std::endl;
 		return;
+	}
+	else if (this->res == this->size - 1) {
+		int * newStack = new int[2 * this->size];
+		for (int i = 0; i < this->size; ++i) {
+			newStack[i] = this->stack[i];
+		}
+		delete[] this->stack;
+		this->stack = newStack;
+		this->size = this->size * 2;
+	}
 	this->stack[++this->res] = sum;
 	std::cout << "push items " << sum << std::endl;
 }
@@ -39,4 +61,9 @@ void Stack::push(int sum) {
 int Stack::pop() {
 	int sum = this->stack[this->res--];
 	return  sum;
+}
+
+Stack::~Stack() {
+	delete[] this->stack;
+	this->stack = NULL;
 }
